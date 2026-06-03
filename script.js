d42298c46e958b98c362cbc7788a72d6e677fe88
sageien <l35makita@hotmail.co.jp>
2026-06-03 16:18:29 +0900

    Add comments about repeated event handlers

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ const reason = document.querySelector("#reason");
 const reasonText = "乾電池は電圧(電気を押し出す力)を持っています。乾電池の＋極と－極を導線でつなぐと、電気の通り道ができ、電気が流れます。";
 
 //2.イベントリスナー設定
+
+//以下3つのイベントは、resultに表示する文言以外が共通している
+//reasonの表示は完全一致
+//現在は練習中のためベタ打ちで進行
 answer.addEventListener('click', function(){
     result.textContent = '正解！';
     reason.textContent = reasonText;
