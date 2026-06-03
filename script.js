0dcd97b54a27cfbaba451226eb116742dfb177dd
sageien <l35makita@hotmail.co.jp>
2026-06-03 14:54:14 +0900

    Add incorrect answer buttons

--- script.js
+++ script.js
@@ -2,8 +2,18 @@
 
 const answer = document.querySelector("#correct-button");
 const result = document.querySelector("#result");
+const water = document.querySelector("#water-button");
+const air = document.querySelector("#air-button");
 
 //2.イベントリスナー設定
 answer.addEventListener('click', function(){
     result.textContent = '正解！';
+});
+
+water.addEventListener('click', function () {
+    result.textContent = '残念！';
+});
+
+air.addEventListener('click', function () {
+result.textContent = '残念！';
 });
