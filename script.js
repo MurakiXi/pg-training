640050fc8c464eb92296292c83ed96c758104574
sageien <l35makita@hotmail.co.jp>
2026-06-03 15:27:30 +0900

    Add reason text

--- script.js
+++ script.js
@@ -4,16 +4,21 @@ const answer = document.querySelector("#correct-button");
 const result = document.querySelector("#result");
 const water = document.querySelector("#water-button");
 const air = document.querySelector("#air-button");
+const reason = document.querySelector("#reason");
+const reasonText = "乾電池は電圧(電気を押し出す力)を持っています。乾電池の＋極と－極を導線でつなぐと、電気の通り道ができ、電気が流れます。";
 
 //2.イベントリスナー設定
 answer.addEventListener('click', function(){
     result.textContent = '正解！';
+    reason.textContent = reasonText;
 });
 
 water.addEventListener('click', function () {
     result.textContent = '残念！';
+    reason.textContent = reasonText;
 });
 
 air.addEventListener('click', function () {
-result.textContent = '残念！';
+    result.textContent = '残念！';
+    reason.textContent = reasonText;
 });
