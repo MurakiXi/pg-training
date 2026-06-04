c7c815f3e0ec9f568c2a5c346b3bb8ef0af61fc7
sageien <l35makita@hotmail.co.jp>
2026-06-04 12:38:26 +0900

    xtract answer display function

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 //1.要素の取得
 
 const answer = document.querySelector("#correct-button");
-const result = document.querySelector("#result");
 const water = document.querySelector("#water-button");
 const air = document.querySelector("#air-button");
+const result = document.querySelector("#result");
 const reason = document.querySelector("#reason");
 const reasonText = "乾電池は電圧(電気を押し出す力)を持っています。乾電池の＋極と－極を導線でつなぐと、電気の通り道ができ、電気が流れます。";
 
-//2.イベントリスナー設定
-
-//以下3つのイベントは、resultに表示する文言以外が共通している
-//reasonの表示は完全一致
-//現在は練習中のためベタ打ちで進行
-answer.addEventListener('click', function(){
-    result.textContent = '正解！';
+//2.関数自作
+function showResult(msg) {
+    result.textContent = msg;
     reason.textContent = reasonText;
+}
+
+//3.イベントリスナー設定
+
+answer.addEventListener('click', function () {
+    showResult('正解！')
 });
 
 water.addEventListener('click', function () {
-    result.textContent = '残念！';
-    reason.textContent = reasonText;
+    showResult('残念！')
 });
 
 air.addEventListener('click', function () {
-    result.textContent = '残念！';
-    reason.textContent = reasonText;
+    showResult('残念！')
 });
