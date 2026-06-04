3671614d27755a634d3cc98bd60c47e62e6a1fa1
sageien <l35makita@hotmail.co.jp>
2026-06-04 14:05:38 +0900

    Pass result and explanation to showResult

--- script.js
+++ script.js
@@ -8,21 +8,21 @@ const reason = document.querySelector("#reason");
 const reasonText = "乾電池は電圧(電気を押し出す力)を持っています。乾電池の＋極と－極を導線でつなぐと、電気の通り道ができ、電気が流れます。";
 
 //2.関数自作
-function showResult(msg) {
+function showResult(msg, explanation) {
     result.textContent = msg;
-    reason.textContent = reasonText;
+    reason.textContent = explanation;
 }
 
 //3.イベントリスナー設定
 
 answer.addEventListener('click', function () {
-    showResult('正解！ よくできました')
+    showResult('正解！', reasonText)
 });
 
 water.addEventListener('click', function () {
-    showResult('残念！　もう一度考えてみよう')
+    showResult('残念！', reasonText)
 });
 
 air.addEventListener('click', function () {
-    showResult('残念！　もう一度考えてみよう')
+    showResult('残念！', reasonText)
 });
