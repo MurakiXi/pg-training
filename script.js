15e694f0db2f04c9569da6db0ed7be54a2e61d90
sageien <l35makita@hotmail.co.jp>
2026-06-05 15:49:53 +0900

    Clean up answer judgement code

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const result = document.querySelector("#result");
 const correctAnswer = '電気';
 const reason = document.querySelector("#reason");
 const reasonText = "乾電池は電圧(電気を押し出す力)を持っています。乾電池の＋極と－極を導線でつなぐと、電気の通り道ができ、電気が流れます。";
+
 //2.関数自作
 function showResult(msg, explanation) {
     result.textContent = msg;
