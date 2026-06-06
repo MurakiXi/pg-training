a9599bd3d0b345f8fca704ecf521da22ae125f17
sageien <l35makita@hotmail.co.jp>
2026-06-06 16:06:26 +0900

    Rebuild single-question quiz for review

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 //1.要素の取得
-
 const answerButtons = document.querySelectorAll(".answer-button");
 const result = document.querySelector("#result");
-const correctAnswer = '電気';
 const reason = document.querySelector("#reason");
-const reasonText = "乾電池は電圧(電気を押し出す力)を持っています。乾電池の＋極と－極を導線でつなぐと、電気の通り道ができ、電気が流れます。";
 
-//2.関数自作
-function showResult(msg, explanation) {
-    result.textContent = msg;
+//2.変数の宣言
+const correctAnswer = "水蒸気";
+const reasonText = "水を熱し続けると、水は気体になって空気中へ出ていきます。この気体になった水を水蒸気と呼びます。ちなみに「湯気」は「水蒸気が空気中で冷やされて細かい水の粒になり、人の目に見えるようになったもの」で、水蒸気とは違うものです。";
+
+
+//3.関数の自作
+function showResult(judge, explanation) {
+    result.textContent = judge;
     reason.textContent = explanation;
 }
 
-//3.イベントリスナー設定
-
+//4.イベントリスナー設定
 answerButtons.forEach(function (answerButton) {
     answerButton.addEventListener("click", function (event) {
         if (event.target.textContent.trim() === correctAnswer) {
