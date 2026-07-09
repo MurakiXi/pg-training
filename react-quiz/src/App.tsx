fd5cd41d43dadde1fa7f61fc7799eba2ddbb09e0
sageien <l35makita@hotmail.co.jp>
2026-07-09 22:10:51 +0900

    feat: render static quiz screen with JSX

--- react-quiz/src/App.tsx
+++ react-quiz/src/App.tsx
@@ -1,12 +1,25 @@
 function App() {
 
   return (
+    <>
+      <header>
+        <div className="header__logo">Science Quiz</div>
+      </header>
       <main>
-          <h1>Science Quiz</h1>
-          <p>
-            React版クイズアプリ
-          </p>
+        <div id="progress">第1問／全3問</div>
+        <div className="question-title">問題：</div>
+        <div id="statement">水を熱し続けると、水は何になって空気中へ出ていくでしょうか？</div>
+        <div className="choices">
+            <button className="answer-button">氷</button>
+            <button className="answer-button">水蒸気</button>
+            <button className="answer-button">砂</button>
+            <button className="answer-button">光</button>
+        </div>
+        <p id="result">ここに結果が表示されます</p>
+        <p id="reason">ここに解説が表示されます</p>
+        <button id="next-button">次の問題</button>
       </main>
+    </>
   )
 }
 
